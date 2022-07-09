519f5736c5237fae450f996c3aba20ddf4dc2f0e
ashusharmasigdev <ashusharma.sigmadev@gmail.com>
2022-07-09 23:46:23 +0530

    readme & release ver

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# OWLDOM
+<div id="top"></div>
+
+<br />
+<div align="center">
+    <a href="https://github.com/Niftproj/owldom">
+        <img src="meta/brand.png" alt="Logo" width="auto" height="147">
+    </a>
+
+  <h3 align="center">OWLDOM</h3>
+
+  <p align="center">
+        Do not mess with DOM/Markups <i>(WORK IN PROGRESS.)</i>
+    <br />
+  </p>
+
+</div>
+
+<br />
+
+<h3>Usage :</h3>
+```js
+<script src="owldom.js"></script>
+<script>
+
+  var domBoy = new OwlDOM();
+
+  /// prototype of `grabElement()`
+  .grabElement(__TAG_NAME__, __ID__, __CLASS_LIST__)
+
+  // below all are optionals, only one can give as expected
+  // __TAG_NAME__: h1, h3, div, etc.
+  // __ID__: id attribute of any element like foo, bar etc
+  // __CLASS_LIST__: [] classNames in array
+
+  // to get all elements as a array even childs
+  console.log(domBoy.grabElement('*'));
+
+  // to get h1 + element with specified Id + elements array with specified classNames
+  console.log(domBoy.grabElement('h1', 'unique-h2'), ['class1', 'class2']);
+
+</script>
+```
--- owldom.js
+++ owldom.js
@@ -1,24 +1,56 @@
-/*
-Easier manage the DOM. Don't mess with it.
+/**
+ * @author Ashu Sharma (ashusharma.sigmadev@gmail.com)
+ * @license MIT
+ * @copyright Copyright 2022 - Nift* (github.com/Niftproj/owldom).
+ */
+
+/**
+* Easier manage the DOM. Don't mess with it.
+* @class OwlDOM
 */
 function OwlDOM() {}
 
+/**
+ * Get Object with metadata (only for inner use of OwlDOM)
+ * @param {String} method by which method (i.e. className, tagName, id) datas are collected
+ * @param {*} data The data that is selected, mainly in Node, NodeList, Array
+ * @returns {Object} Returns an Object of given parameters 'method' and 'data'
+ */
 function getRoundObject(method, data)
 {
     return {method: method, data: data};
 }
 
-/// roundSearch[0] -> by Tag Name:[] | [1] -> by id:DOM, [2] -> by class Lists:[]
-OwlDOM.prototype.grabElement = function({tagName, id, classList: []})
+/**
+ * Check the given value is really given or not (only for inner use of OwlDOM)
+ * @param {String} data The data that has to be runned checks
+ * @returns {Boolean} Returns boolean value for checks passed or not
+ */
+function valueExist(data)
+{
+    if(data !== undefined && data !== '' && data !== null)
+        return true;
+    return false;
+}
+
+/**
+ * Grab any element of DOM
+ * @example .grabElement('*') to get All elements even childs in array
+ * @param {String} tagName to find elements with provided tagName (Optional)
+ * @param {String} id to find elements with specified Id (Optional)
+ * @param {Array[String,String]} classList to select all elements with given classList (Optional)
+ * @returns {Array} Returns an Array of {getRoundObject} function with metadata
+ */
+OwlDOM.prototype.grabElement = function(tagName, id, classList = [])
 {
     let roundSearch = [];
 
-    if(tagName !== null)
+    if(valueExist(tagName))
     {
         roundSearch.push(getRoundObject('tagName', document.getElementsByTagName(tagName)));
     }
 
-    if(id !== null)
+    if(valueExist(id))
     {
         roundSearch.push(getRoundObject('id', document.getElementById(id)));
     }
@@ -26,7 +58,7 @@ OwlDOM.prototype.grabElement = function({tagName, id, classList: []})
     if(classList.length > 0)
     {
         classList.forEach(classN => {
-            roundSearch.push(getRoundObject('className', document.querySelectorAll(classN)));
+            roundSearch.push(getRoundObject('className', document.querySelectorAll('.'+classN)));
         });
     }
 
